88ccd02219d068fe1d2393856b90b63a4133b14b
CodinghakdangDaniel <junh2e97@gmail.com>
2023-10-04 01:04:42 +0900

    lab20

---
+++ script.js
@@ -0,0 +1,53 @@
+let inputValue = document.getElementsByClassName('search-box')[0]
+const showBtn = document.getElementById('show-btn')
+const itemBox = document.getElementById('item-box')
+
+// async function getItems() {
+//     try {
+//         const response = await fetch('https://fakestoreapi.com/products')
+//         console.log(response)
+        
+//         const data = await response.json()
+//         console.log(data)
+        
+//         data.forEach((result) => {
+//             const image = document.createElement('img')
+//             image.src = result.image
+//             itemBox.appendChild(image)
+//         })
+//     }
+
+//     catch (error) {
+//         console.error(error)
+//     }
+// }
+
+async function getItems() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        console.log(response)
+        
+        const data = await response.json()
+        console.log(data);
+        
+        const valueNumber = Number(inputValue.value)
+        const image = document.createElement('img')
+        console.log('입력값', valueNumber)
+
+        for (let i= 0; i < data.length; i++) {
+            console.log('콘솔i', i);
+            console.log('i의 평점', data[i].rating.rate)
+            
+            if (data[i].rating.rate >= valueNumber) {
+                image.src = data[i].image
+                itemBox.appendChild(image)
+            }
+        }
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+
+showBtn.addEventListener('click', getItems)
